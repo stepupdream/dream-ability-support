cafc336933a50468c4a7167f5c2a0814baf70912
stepupdream <stepupdream1002@gmail.com>
2024-12-30 20:50:39 +0900

    Refactor workflows and remove redundant doc comments.

--- src/Supports/File/YamlFileOperation.php
+++ src/Supports/File/YamlFileOperation.php
@@ -152,8 +152,6 @@ class YamlFileOperation extends FileOperation
     /**
      * Extract the file extension from a file path.
      *
-     * @param  string  $path
-     * @return string
      * @see \Illuminate\Filesystem\Filesystem::extension
      */
     protected function extension(string $path): string
